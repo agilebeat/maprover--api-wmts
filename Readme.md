f4e122b91a7cebdb35ef0de5f7d8c11dd28dcc3c
Marek Dwulit <marek.dwulit@agilebeat.com>
2019-08-11 22:15:06 -0400

    WORKING: Tested, API Gateway turns respone when *.png to binary otherwise returns 302

---
+++ Readme.md
@@ -0,0 +1,71 @@
+# Install two-bucket-lambda-infer
+
+
+### Preparing Deployment environment
+
+To deploy lambda function we use docker container. Check out the docker 
+project from github: https://github.com/agilebeat-inc/docker-serverless-aws-client
+
+Follow instructions in the Readme.md file for the docker project
+
+### Prepering serverless environment on the docker container
+
+Before we deploy lambda function to the aws we need to execute few preparation 
+steps. First will be creating virtual environment for python 3.6 where we will
+install all packages necessary for the lambda function. Then we will install 
+serverless plugins necessary for deployment (plugins are extensions allowing more granular
+control over aws). Finally we will deploy lambda function.
+
+At this point we are assuming that you have executed the steps listed in the
+Readme.md for the docker-serverless-aws-client project. You should have running 
+docker container and have shell terminal: `root@<IMAGE-ID>:/classify-lambda#`
+
+#### Step 1. Set pu virtual python environment
+
+1. Run: `python3 -m venv python.venv`
+   This comman will create python virtual environment which is a folder `python.venv`
+   insde the project. Please don't check in that folder to the github.
+   
+2. Activate python virtual environment. Run command:
+
+   `source python.venv/bin/activate`
+   
+   You should see:
+   
+   `(python.venv) root@<IMAGE-ID>:~/classify-lambda#`
+   
+3. Install packages necessary for lambda in the virtual environment:
+
+    None (You don't have to install anything)
+   
+4. Capture python packages in a file so that serverless will know which 
+   python packages should be included in zip file containing lambda function
+   
+   `pip freeze > requirements.txt`
+   
+5. Install necessary plugins for serverless. Run:
+   
+   `serverless plugin install -n serverless-python-requirements`
+   
+   `serverless plugin install -n serverless-reqvalidator-plugin`
+   
+   `serverless plugin install -n serverless-aws-documentation`
+   
+   `serverless plugin install -n serverless-plugin-custom-roles`
+   
+   `npm install --save-dev serverless-apigw-binary`
+   
+   `npm install --save-dev serverless-apigwy-binary`
+   
+   Comment: *Watch out two last commands have only one letter difference in module names
+   run both.*
+   
+   *Comment: Serverless plugins are extensions for serverless allowing to get more granula
+   control for different cloud providers: aws, asure ...*
+
+  
+6. Finally run deploy command:
+
+   `serverless deploy -v`
+   
+   
---
+++ wmts.py
@@ -0,0 +1,93 @@
+try:
+  import unzip_requirements
+  import json
+  import boto3
+  from botocore.errorfactory import ClientError
+  import base64
+  import string
+  import random
+  from io import BytesIO
+except ImportError:
+  pass
+
+
+
+        
+def select_server():
+    servers = 'abc'
+    return random.choice(servers)
+
+def get_redirect(z, x, y):
+    response = {
+        "statusCode": 302,
+        "headers": {'Location': 'https://' +
+                                select_server() +
+                                '.tile.openstreetmap.org/' +
+                                z + '/' +
+                                x + '/' +
+                                y,
+                    'Access-Control-Allow-Origin': '*'}
+    }
+    return response
+
+def construct_debug_response(z, x, y, e):
+    response = {
+        "statusCode": 200,
+        "headers": {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+        "body": json.dumps({'Exception': str(e), 'key': str(z) +'/'+ str(x) + '/' + str(y)})
+    }
+    return response
+
+def read_tile(z, x, y):
+    tile_64 = 'Initial String'
+    response = None
+    try:
+        client = boto3.client('s3')
+        tile = client.get_object(Bucket='wmts-maprover', Key=str(z) +'/'+ str(x) + '/' + str(y))
+        tile_64 = base64.b64encode(BytesIO(tile['Body'].read()).read()).decode('UTF-8')
+    except Exception as e:
+        response = get_redirect(z, x, y)
+    else:
+        response = {
+            "isBase64Encoded": True,
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "image/png",
+                'Access-Control-Allow-Origin': '*'
+            },
+            "body": tile_64
+        }
+    return response
+
+def get_tile(z, x, y):
+    body = "iVBORw0KGgoAAAANSUhEUgAAAQAAAAEACAYAAABccqhmAAADYklEQVR4nO3UMWrDUBBAQZ/bKXUD3UKVA+q+DiPfRKlcxIFg3BjxpphmYWGbfZf7vh9A0+XTBwCfIwAQJgAQJgAQJgAQJgAQJgAQJgAQJgAQJgAQJgAQJgAQJgAQJgAQJgAQJgAQJgAQJgBwctsYb+8KAJzUNsYxTdMxz/Ov+bIsx9f1+lIYBABO6vHgzwHYxjjWdRUAKHgOwH3fBQAqBADCBADCBADCvm+3P7NtDAEA/icAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAECYAEPYDw8wlgMsU1kgAAAAASUVORK5CYII="
+
+    response =  {
+        "isBase64Encoded": True,
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "image/png",
+        },
+        "body": body
+    }
+    return response
+
+def wmtsHandler(event, context):
+    z = event['pathParameters']['z']
+    x = event['pathParameters']['x']
+    y = event['pathParameters']['y']
+
+    response = read_tile(z, x, y)
+    return response
+
+
+
+
+
+
+
+
+
+
+
+
